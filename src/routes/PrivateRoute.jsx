2db1d27c7c419e8e4870395064b8e9348e183558
Versha K <versha@lapsunr08.successivetech.com>
2019-10-10 19:12:41 +0530

    Modified code commit

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import DashboardRoute from '../layouts/PrivateLayout/PrivateLayout';
 import TextFieldDemo from '../pages/TextFieldDemo/TextFieldDemo';
 import Trainee from '../pages/Trainee/Trainee';
 import InputDemo from '../pages/Inputdemo/inputDemo';
 import Math from '../components/Math/Math';
+import NoMatch from '../pages/NoMatch/NoMatch';
 
 const PrivateRoute = ()=> {
     return(
         <Router>
+        <Route path='*' exact={true} component={NoMatch} />
         <div>
         <Switch>
           <DashboardRoute path="/textFieldDemo" component={TextFieldDemo} />
