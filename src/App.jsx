d491cc6775a156e8d619cea16d051ba2bb9f3ca2
varsha-22 <55829276+varsha-22@users.noreply.github.com>
2019-10-16 17:12:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-// import TraineeList from './pages/Trainee/TraineeList';
 import Routes from "./routes/Routes";
-import SnackBarProvider from "./SnackBarProvider/SnackBarProvider";
 const App = () => {
   return (
     <div>
       <Routes />
-      <SnackBarProvider />
     </div>
   );
 }
