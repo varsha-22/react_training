0ca1fb88859ea4427451fc74fe472d1942f65f0c
varsha-22 <55829276+varsha-22@users.noreply.github.com>
2019-10-21 16:19:15 +0530

    Update api.js

--- src/lib/utils/api.js
+++ src/lib/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `http://jsonplaceholder.typicode.com/`
   baseURL: `https://express-training.herokuapp.com/api/`,
   headers: {
     'Content-Type': 'application/json'
